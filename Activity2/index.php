1074580e652445c8de500989e09c7ad392b28da2
Erikka Marielle M. Enaje <142382057+ErikkaEnaje@users.noreply.github.com>
2024-04-24 19:10:24 +0800

    Add files via upload

---
+++ Activity2/index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>3BSCS-2||ENAJE||ACT2</title>
+ 
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<script
+  src="https://code.jquery.com/jquery-3.7.1.js"
+  integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+  crossorigin="anonymous"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   
+  </head>
+    <body>
+      <h1 class="title">Hello World! </h1>
+      <p id="currentTime"></p>
+      <script src="script.js"></script>
+
+       
+        <div class="btn-group">
+          <button class =" btn btn-info">Home</button>
+          <button class =" btn btn-info">About Us</button>
+          <button class =" btn btn-info">Contact us</button>
+          <button class =" btn btn-info">Products</button>
+        </div>
+       
+        <div class="btn-group-vertical">
+          <button class =" btn btn-info">Home</button>
+          <button class =" btn btn-info">About Us</button>
+          <button class =" btn btn-info">Contact us</button>
+          <button class =" btn btn-info">Products</button>
+        </div>
+		
+		
+		<div class="btn-group btn-group">
+     
+    <button class="btn btn-link"> Click Me </button>
+    <button class="btn btn-danger btn-block"> Click Me</button>
+    <button class="btn btn-danger disabled"> Click Me</button>
+    <button class="btn btn-danger active">Click Me</button>
+    </div>
+		
+		
+		
+	   <div class="container-fluid">
+        <img src="https://images.pexels.com/photos/3640930/pexels-photo-3640930.jpeg?cs=srgb&dl=pexels-ena-marinkovic-3640930.jpg&fm=jpg" width="200" height="200">
+        <img src="https://images.pexels.com/photos/4327602/pexels-photo-4327602.jpeg?cs=srgb&dl=pexels-jess-loiterton-4327602.jpg&fm=jpg" class="img-rounded" width="200" height="200">
+        <img src="https://images.pexels.com/photos/6618562/pexels-photo-6618562.jpeg?cs=srgb&dl=pexels-mariam-antadze-6618562.jpg&fm=jpg" class="img-circle" width="200" height="200">
+        <img src="https://images.pexels.com/photos/14448266/pexels-photo-14448266.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="img-thumbnail" width="200" height="200">
+       
+        </div>
+	  
+   
+   
+   <a href="#">Message<span class="badge">10<span></a><br/>
+     <a href="#">Notification<span class="badge">30</span></a>
+     <buttom class="btn btn-primary">Click to see Notifications<span class="badge">500</span></buttom>
+     <div class="page-header">
+       <h3>Download <span class="label label-default">Version 10.1</span></h3>
+     </div>
+	 
+	 
+	 
+   
+        <div class"container-fluid">
+             <div class="dropdown">
+             <button class="btn btn-primary dropdown-toggle" data-toggle-"dropdown">college<span class="caret"></span></button>
+             <ui class="dropdown-menu">
+               <li><a href="#">CCS</a></li>
+               <li><a href="#">CET</a></li>
+               <li><a href="#">CBA</a></li>
+               <li class="divider"></li>
+               <li class="dropdown-header">Exit</li>
+               <li><a href="#">Logout</a></li>
+        </ui>
+        </div>
+       
+        <div class="container-fluid">
+          <span class="glyphicon glyphicon-trash"></span>:Trash Can <br/>
+          <span class="glyphicon glyphicon-heart"></span>:Heart <br/>
+          <button class="btn btn-success"><span class = " glyphicon glyphicon-star"></span></button>
+          <button class="btn btn-info"><span class = "glyphicon glyphicon-music"></span></button>
+        </div>
+       
+       <div class="dropup">
+            <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+            <ul class="dropdown-menu">
+              <li><a href="#">CCS</a></li>
+              <li><a href="#">CET</a></li>
+              <li><a href="#">CBA</a></li>
+              <li class="divider"></li>
+              <li class="dropdown-header">Exit</li>
+              <li><a href="#">Logout</a></li>
+          </ul>
+          </div>
+        </div>
+       
+      <div class="container-fluid">
+        <div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+     <div class="alert alert-Sucess"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+     <div class="alert alert-warning"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+     <div class="alert alert-info"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+     </div>
+	 
+     
+       
+      <div class="container-fluid">
+        <div class="jumbotron">
+          <h1>Heading in jumbtron</h1>
+        </div>
+        <h1>Heading without jumbtron</h1>
+        <div class="page-header">
+          <h1>Welcome</h1>
+        </div>
+       
+        <p class="well well-sm">This is the well</p>
+        <p class="well">This is the well</p>
+        <p class="well well-lg">This is the well</p>
+      </div>
+   
+    <div class="container fluid">
+      <button class="btn btn-default">Click Me</button>
+    </div>
+  </body>
+</html>
+
+       
+    </body>
+</html>
